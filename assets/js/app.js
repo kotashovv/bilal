7cbbbfbf029612ca9beeb93a3fd86275762b75fc
Russel <kotashov03@gmail.com>
2025-03-31 00:47:04 +0400

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
-document.addEventListener('click', ()=>{
+document.addEventListener('DOMContentLoaded', ()=>{
     const reviewsSlider = new Swiper('.reviews__slider', {
         slidesPerView: 3,
         spaceBetween: 15,
     })
+
+
+    const sertSlider = new Swiper('.docs__slider',  {
+        slidesPerView: 4,
+        spaceBetween: 15,
+    })
+
+    Fancybox.bind("[data-fancybox]", {
+        // Your custom options
+      });
 })
